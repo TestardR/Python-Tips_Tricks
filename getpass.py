fbec6d281284e786f7ce1a9beb00fec767e8124f
unknown <rmtestard@gmail.com>
2019-04-23 11:06:33 +0200

    getpass

---
+++ getpass.py
@@ -0,0 +1,13 @@
+from getpass import getpass
+
+# wrong way
+# username = input('Username: ')
+# password = input('Password: ')
+
+# print('Logging In...')
+
+# with getpass
+username = input('Username: ')
+password = getpass('Password: ')
+
+print('Logging In...')
