eed7e006e9dad44e9481c924254899e4e1a0dfbd
unknown <rmtestard@gmail.com>
2019-04-23 10:35:08 +0200

    enumerate

---
+++ enumerate.py
@@ -0,0 +1,10 @@
+names = ['Corey', 'Chris', 'Dave', 'Travis']
+
+# not clean
+# index = 0
+# for name in names:
+#     print(index, name)
+#     index += 1
+
+for index, name in enumerate(names, start=1):
+    print(index, name)
